c167239e1df99821e1ce7bea533775aff68cf97a
Tejas Yadav <ytejas1701@kgpian.iitkgp.ac.in>
2024-08-28 23:47:16 +0530

    fixed dashboard alignment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,22 +7,29 @@
   import StreakWidget from "$lib/widgets/StreakWidget.svelte";
 </script>
 
-<ColContainer>
-  <RowContainer>
-    <ProfileWidget username={"Anomander"} dateJoined={"14 Feb 2024"} daysActive={125} rank={135848}/>
-    <ColContainer>
-      <StreakWidget streak={24}/>
-      <RowContainer>
-        <CountWidget label={"kanji"} count={23}/>
-        <CountWidget label={"vocab"} count={125}/>
-      </RowContainer>
-    </ColContainer>
-    <ActiveDaysWrapper/>
-  </RowContainer>
-  <!-- <RowContainer>
-    <KanjiWrapper kanji={"水"} size={2}/>
-    <KanjiWrapper kanji={"水"} size={2}/>
-    <KanjiWrapper kanji={"水"} size={2}/>
-    <KanjiWrapper kanji={"水"} size={2}/>
-  </RowContainer> -->
-</ColContainer>
+<wrapper>
+  <ColContainer>
+    <RowContainer>
+      <ProfileWidget username={"Anomander"} dateJoined={"14 Feb 2024"} daysActive={12} rank={1}/>
+      <ColContainer>
+        <StreakWidget streak={24}/>
+        <RowContainer>
+          <CountWidget label={"kanji"} count={6}/>
+          <CountWidget label={"vocab"} count={4}/>
+        </RowContainer>
+      </ColContainer>
+    </RowContainer>
+  </ColContainer>
+</wrapper>
+
+<style>
+  wrapper {
+    width:fit-content;
+    height:fit-content;
+    position:absolute;
+    display:block;
+    top:50%;
+    left: 50%;
+    transform:translate(-50%, -50%)
+  }
+</style>
